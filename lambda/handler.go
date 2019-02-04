2c7de2c641fbbcaf4de2c80dc4de08e1c82d5212
Dave King <dave@projectsbyif.com>
2019-02-04 18:49:02 +0000

    Handle S3 events

--- lambda/handler.go
+++ lambda/handler.go
@@ -3,15 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, s3Event events.S3Event) {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
 }
 
 func main() {
