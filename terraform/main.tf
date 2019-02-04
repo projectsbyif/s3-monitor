c9e099befec8261107c2e6800ce830966f4cc8af
Dave King <dave@projectsbyif.com>
2019-02-04 15:08:32 +0000

    Create S3 bucket and CloudTrail

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+resource "aws_cloudtrail" "trail" {
+  name                  = "verifiable-trail"
+  s3_bucket_name        = "${aws_s3_bucket.logs.id}"
+  is_multi_region_trail = true
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket        = "${var.bucket_name}"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::${var.bucket_name}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
