a2bc4c1dcffa7f609298462aefe83a3f7284f27e
Emily Mattiussi <emily.mattiussi@gmail.com>
2019-02-05 15:08:10 +0000

    Create S3 bucket notification.

---
+++ terraform/bucket_notification.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.func.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.logs.arn}"
+}
+
+resource "aws_lambda_function" "func" {
+  filename      = "${var.filename}"
+  function_name = "${var.function_name}"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "${var.handler}"
+  runtime       = "go1.x"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.logs.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.func.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+    filter_suffix       = ".json"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,18 @@
 variable "bucket_name" {
   type = "string"
 }
+
+# Lambda function zip filename
+variable "filename" {
+  type = "string"
+}
+
+# Lambda function name
+variable "function_name" {
+  type = "string"
+}
+
+# Lambda handler name
+variable "handler" {
+  type = "string"
+}
