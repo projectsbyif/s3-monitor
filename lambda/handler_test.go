f5a219c753384e6286fa19565600b84dbc18b8de
Emily Mattiussi <emily.mattiussi@gmail.com>
2019-02-26 16:51:06 +0000

    Refactored handler_test
    Added new test cases for no events and multiple events. Moved repeated code (which asserted expected number of leaves in tree) into separate function assertLeavesAdded. Added test runs for each case.

--- lambda/handler_test.go
+++ lambda/handler_test.go
@@ -14,13 +14,36 @@ import (
 	stestonly "github.com/google/trillian/storage/testonly"
 )
 
-func TestHandleRequest(t *testing.T) {
-	s, _ := ioutil.ReadFile("testdata/event.json")
-	ctx := context.Background()
+var (
+	treeID int64
+)
+
+func loadS3Event(t *testing.T, fileName string) events.S3Event {
 	var inputEvent events.S3Event
+	s, _ := ioutil.ReadFile(fileName)
 	if err := json.Unmarshal([]byte(s), &inputEvent); err != nil {
 		t.Errorf("could not unmarshal event. details: %v", err)
 	}
+	return inputEvent
+}
+
+func numberOfLeaves(tx storage.LogMetadata) int64 {
+	trees, _ := tx.GetUnsequencedCounts(context.TODO())
+	return trees[treeID]
+}
+
+func assertLeavesAdded(t *testing.T, tx storage.LogMetadata, expectedNumberOfNewLeaves int64, f func()) {
+	countBefore := numberOfLeaves(tx)
+	f()
+	countAfter := numberOfLeaves(tx)
+	if countAfter-countBefore != expectedNumberOfNewLeaves {
+		t.Errorf("Expected %v leaf in tree, found %v", expectedNumberOfNewLeaves, countAfter-countBefore)
+	}
+}
+
+func TestHandleRequest(t *testing.T) {
+	// Setup code
+	ctx := context.Background()
 	sp, _ := server.NewStorageProvider("memory", nil)
 	var tree *trillian.Tree
 	sp.AdminStorage().ReadWriteTransaction(ctx, func(ctx context.Context, tx storage.AdminTX) error {
@@ -28,14 +51,30 @@ func TestHandleRequest(t *testing.T) {
 		tx.Commit()
 		return nil
 	})
-	treeID := tree.TreeId
+	treeID = tree.TreeId
 	logServer, _ := StartTrillian(ctx, sp, treeID)
 	handleRequest := CreateHandler(logServer, treeID)
-	handleRequest(ctx, inputEvent)
 	tx, _ := sp.LogStorage().Snapshot(ctx)
 	defer tx.Close()
-	trees, _ := tx.GetUnsequencedCounts(ctx)
-	if trees[treeID] != 1 {
-		t.Errorf("Expected 1 leaf in tree, found %v", trees[treeID])
-	}
+
+	t.Run("one event", func(t *testing.T) {
+		assertLeavesAdded(t, tx, 1, func() {
+			inputEvent := loadS3Event(t, "testdata/oneEvent.json")
+			handleRequest(ctx, inputEvent)
+		})
+	})
+
+	t.Run("no events", func(t *testing.T) {
+		assertLeavesAdded(t, tx, 0, func() {
+			inputEvent := loadS3Event(t, "testdata/noEvent.json")
+			handleRequest(ctx, inputEvent)
+		})
+	})
+
+	t.Run("multiple events", func(t *testing.T) {
+		assertLeavesAdded(t, tx, 5, func() {
+			inputEvent := loadS3Event(t, "testdata/fiveEvents.json")
+			handleRequest(ctx, inputEvent)
+		})
+	})
 }
